4a416ae609be5939177ad101a090b0dc32642bc6
The Wizard of Hahz <thewizardofhahz@king-hahz.attlocal.net>
2023-03-03 06:40:23 -0500

    update

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'oxygenator.myshopify.com',
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hahzcandy.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '70faab4b482211c4167f94181a4ba4ed',
+      '67d3ddf6baff758b3ffad350488de1d0',
     storefrontApiVersion: '2022-07',
     // @ts-ignore
     storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
